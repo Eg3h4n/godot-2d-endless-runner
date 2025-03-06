d5ad9777a18169f744cc60497f939e1b6a32b46b
Egehan Gur <53318302+Eg3h4n@users.noreply.github.com>
2025-03-06 14:48:11 +0300

    start screen and pause menu added

--- ui/pause_menu_screen.gd
+++ ui/pause_menu_screen.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
-
+func _ready():
+	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if GameInputEvents.pause_input() and get_tree().paused:
